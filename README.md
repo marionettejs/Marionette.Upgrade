c811b9f26a7ef2dae39d8f3fe3232a8fa5af3ede
Jason Laster <jason.laster.11@gmail.com>
2014-04-28 22:41:50 -0400

    Switch to codemod version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Marionette Upgrade
+
+We made lots of changes to Marionette's API in 2.0. In many ways, we're taking out the garbage that accumulated over the past couple years. With all of these changes, it's easy to wonder if they'll really help or if it's worth it to take the time to fix them. I think it is, there are lots of great projects planned for 2.x that if you're a fan of Marionette you'll want to enjoy.
+
+Even though we made lots of changes to the API, the vast majority of them were simple renamings of methods, events and properties. This tool will help walk you through all of the changes.
+
+The way it works is a lot like `git add -p`. When you run the tool, you'll be shown each change and prompted to approve it, reject it, or edit it.
+
+I realize that this is still a scary process because you'll be covering a lot of ground fairly quickly and the final diff will still be large. I hope that this tool will be a good starting off point. If you're like me, you'll quickly start playing with the regular expressions to fine toon the searches for your project and approach this change-set as methodically as possible in whatever way works out for you.
+
+Lastly, this is an experiment in coming up with the right workflow for organizing a large API upgrade. The tool is decidedly very simple, so that it's flexible and can change with time. If you find something works particulary well, please share your successes. If you find that there's a glaring hole, please don't be a stranger.
+
+### How to get set up
+1. clone this repo locally
+2. open `upgrade.py` and change the repo variable to point to your project
+3. run upgrade.py with this command `python upgrade.py`
+
+### Additional Information
+
++ The upgrade tool is organized in three categories based on the type of change. By default, they're all turned on, but it's suggested that you do one change at a time.
++ There's nothing magical here. I wrote each of these changes to work off of simple regexes, if there are additional changes that you think would help feel free to add them. Also, if there are places where I missed things, please file a Pull Request!
++ This works. I know, I was surprised too. I ran this tool against our v1.8 test suite and it faired surprisingly well. It made over 500 changes and only missed a couple things:
+	+ methods that referenced with spies "foobar" instead of "foobar("
+	+ semantic changes like onBeforeDestroy, which no longer work the same way
+	+ function parameter change like appendHtml losing a param. we could write a regex for this, but meh.
+
+
+### Changes
+
+The two big changes in v2.0 are the change in collection view from itemViews to childViews and the change in Views going from closing to destroying.
+
+The childView change should be relatively simple to search out and change and none of the semantics changed.
+
+The close to destroy view change should also be easy to find, but unlike childView the behavior of destroy is different from close, when a view was closed it could be re-opened, now that view's are being destroyed there's no bringing them back. So, when you change your code to start destroying your views, you should double check that you don't try re-open them again.
---
+++ upgrade.py
@@ -0,0 +1,178 @@
+from sub import *
+
+repo = "../backbone.marionette/spec/javascripts"
+
+renameCloseToDestroy = True
+renameLayoutToLayoutView = True
+renameItemToChild = True
+apiCleanup = True
+
+
+import sys
+
+
+codemod = "./bin/codemod.py"
+
+def confirm():
+  ch = sys.stdin.read(1)
+  if ch == "\n" or ch == "y":
+    return True
+  if ch == "n":
+    return False
+  elif ch == "q":
+    sys.exit()
+  else:
+    print "sorry, didn\'t understand"
+
+def sub(search, replace):
+  print "Would you like to replace: %s for %s? [y,n,q]" % (search, replace)
+  good = confirm()
+  global repo
+
+  if good:
+    cmd = ("%s -m -d %s %s %s" % (codemod, repo, search, replace))
+    print cmd
+    system(cmd)
+
+
+def subString(search, replace):
+  searchTermS = "\"\'%s\'\"" % (search)
+  replaceTermS  = "\"\'%s\'\"" % (replace)
+  searchTermD = '\'\"%s\"\'' % (search)
+  replaceTermD  = '\'\"%s\"\'' % (replace)
+  sub(searchTermS, replaceTermS)
+  sub(searchTermD, replaceTermD)
+
+def subMethod(search, replace):
+  searchTerm = "'.%s\\('" % (search)
+  replaceTerm  = "'.%s('" % (replace)
+  sub(searchTerm, replaceTerm)
+
+def subEvent(search, replace):
+  searchTermCbk = "\"%s\"" % (eventToCbk(search))
+  replaceTermCbk = "\"%s\"" % (eventToCbk(replace))
+
+  sub(searchTermCbk, replaceTermCbk)
+  subString(search, replace)
+
+
+def subTerm(search, replace):
+  searchTerm = "\"%s\"" % search
+  replaceTerm = "\"%s\"" % replace
+  sub(searchTerm, replaceTerm)
+
+def subString(search, replace):
+  searchTerm = "\"%s\"" % search
+  replaceTerm = "\"%s\"" % replace
+  sub(searchTerm, replaceTerm)
+
+def subKey(search, replace):
+  searchTerm = "\"%s:\"" % search
+  replaceTerm = "\"%s:\"" % replace
+  sub(searchTerm, replaceTerm)
+
+def eventToCbk(event):
+  parts = event.split(':')
+  parts = [part.capitalize() for part in parts]
+  cbk = "on"+ ''.join(parts)
+  return cbk
+
+
+
+
+#
+#
+#
+#
+#
+# UPGRADER CHANGES
+#
+#
+#
+#
+#
+#
+
+
+
+
+if (renameCloseToDestroy):
+  subMethod("close", "destroy")
+  subMethod("closeRegions", "destroyRegions")
+  subMethod("closeChildren", "destroyChildren")
+
+  subEvent('collection:before:close', 'before:destroy')
+  subEvent('collection:closed', 'destroy')
+  subEvent('item:before:close', 'before:destroy')
+  subEvent('item:closed', 'destroy')
+  subTerm("closed", "destroy")
+  subEvent("close", "destroy")
+
+  subTerm('isClosed', 'isDestroyed')
+
+  subKey('preventClose', 'preventDestroy')
+
+if (renameLayoutToLayoutView):
+  subTerm("Marionette.Layout\.", "Marionette.LayoutView.")
+  subTerm("Marionette.Layout\(", "Marionette.LayoutView(")
+
+
+if (renameItemToChild):
+  subTerm("itemView", "childView")
+  subTerm("itemViewEventPrefix", "childViewEventPrefix")
+  subTerm("itemViewOptions", "childViewOptions")
+  subTerm("itemEvents", "childEvents")
+
+  subMethod("addChildView", "onChildAdd")
+  subMethod("removeItemView", "onChildRemove")
+  subMethod("getItemView", "getChildView")
+  subMethod("addItemView", "addChild")
+  subMethod("removeItemView", "removeChildView")
+  subMethod("renderItemView", "renderChildView")
+  subMethod("closeChildren", "destroyChildren")
+  subMethod("resetItemViewContainer", "resetChildViewContainer")
+  subMethod("getItemViewContainer", "getChildViewContainer")
+
+  subEvent("before:item:added", "before:add:child")
+  subEvent("after:item:added", "add:child")
+  subEvent("before:item:rendered", "before:render")
+  subEvent("item:rendered", "render")
+  subEvent("item:before:render", "before:render")
+  subTerm("itemview", "childview")
+  subTerm("onItemview", "onChildview")
+
+if (apiCleanup):
+  subMethod("renderModel", "_renderRoot")
+  subEvent("region:add", "add:region")
+  subEvent("region:remove", "remove:region")
+  subEvent("initialize:before", "before:start")
+  subEvent("initialize:after", "start")
+  subTerm("regionType", "regionClass")
+
+  subEvent("composite:collection:before:render", "before:render:template")
+  subEvent("composite:model:rendered", "render:template")
+  subEvent("composite:rendered", "render")
+  subEvent("composite:before:render:collection", "before:render:collection")
+  subEvent("composite:collection:rendered", "render:collection")
+
+  subEvent("collection:before:destroy", "before:destroy:collection")
+  subEvent("collection:destroy", "destroy:collection")
+  subEvent("collection:before:render", "before:render:collection")
+  subEvent("collection:rendered", "render")
+  subEvent("before:child:added", "before:add:child")
+  subEvent("after:child:added", "add:child")
+  subEvent("before:child:remove", "before:remove:child")
+  subEvent("child:removed", "remove:child")
+  subEvent("region:add", "add:region")
+  subEvent("region:remove", "remove:region")
+
+
+# THINGS WE MIGHT NOT BE ABLE TO CHANGE
+# this is a todo list of things I found when working on adding regex
+# that were not immediately obvious to change. Sorry, you'll have to do these yourself :)
+#
+#
+# 1. Module's initialize function now accepts these params(options, moduleName, app) it used to be these (moduleName, app, options)
+# 2. regions need to have an element when they're showing a view. Previously you could show a view in a region and if the region didn't have an element on the page at the time, nothing would happen. Now we through an error so you know immediately that you need to fix something.
+# 3. collectionView appendHtml now accepts two arguments instead of three. Previously, we were passing the collectionview in as the first param. That was redundant so we removed it.
+# I'm sure there other changes as well, so don't treat this as an exhaustive list.
